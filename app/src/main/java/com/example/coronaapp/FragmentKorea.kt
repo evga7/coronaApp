470d6b3a95603b60781aeb13f0a2ff533d7bd197
seounjin <invanda7@gmail.com>
2020-03-17 19:14:07 +0900

    ex

--- app/src/main/java/com/example/coronaapp/FragmentKorea.kt
+++ app/src/main/java/com/example/coronaapp/FragmentKorea.kt
@@ -10,11 +10,7 @@ class FragmentKorea : Fragment() {
     // first
     // 프레그먼트 재생성 시 세팅 해놓은 번들을 통해 다시 넘어오게 할 수 있음.
 
-    // 프레그먼트는 메모리 확보를 위해 파기 될 수 있는데 이때 보통
-    // onSaveInstanceState()를 통해서 데이터를 보관하게 됨
-    // 하지만 newInstance 를 통하여 세팅되면 재생성 시 번들을 통하여
-    // 다시 세팅되어 넘어오게 되는데 그렇게 되게 하기 위하여
-    // 우선 작업?처리?를 아래와 같이 해놓은 것임.
+
     companion object {
         fun newInstance(): FragmentKorea {
             val FragmentKorea = FragmentKorea()
@@ -30,3 +26,9 @@ class FragmentKorea : Fragment() {
 
 
 }
+
+// 프레그먼트는 메모리 확보를 위해 파기 될 수 있는데 이때 보통
+// onSaveInstanceState()를 통해서 데이터를 보관하게 됨
+// 하지만 newInstance 를 통하여 세팅되면 재생성 시 번들을 통하여
+// 다시 세팅되어 넘어오게 되는데 그렇게 되게 하기 위하여
+// 우선 작업?처리?를 아래와 같이 해놓은 것임.
