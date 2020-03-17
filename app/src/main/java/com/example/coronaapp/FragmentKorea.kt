89f5ecf8e1c78888054d7b129076d24526c988bd
seounjin <invanda7@gmail.com>
2020-03-17 19:08:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/coronaapp/FragmentKorea.kt
@@ -0,0 +1,32 @@
+package com.example.coronaapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentKorea : Fragment() {
+    // first
+    // 프레그먼트 재생성 시 세팅 해놓은 번들을 통해 다시 넘어오게 할 수 있음.
+
+    // 프레그먼트는 메모리 확보를 위해 파기 될 수 있는데 이때 보통
+    // onSaveInstanceState()를 통해서 데이터를 보관하게 됨
+    // 하지만 newInstance 를 통하여 세팅되면 재생성 시 번들을 통하여
+    // 다시 세팅되어 넘어오게 되는데 그렇게 되게 하기 위하여
+    // 우선 작업?처리?를 아래와 같이 해놓은 것임.
+    companion object {
+        fun newInstance(): FragmentKorea {
+            val FragmentKorea = FragmentKorea()
+            val args = Bundle()
+            FragmentKorea.arguments = args
+            return FragmentKorea
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_korea, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/coronaapp/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.coronaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private var content: FrameLayout? = null
+
+    private val ItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when(item.itemId){
+            R.id.korea->{
+                val fragment = FragmentKorea.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.world->{
+                val fragment = FragmentWorld()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.mask->{
+                val fragment = FragmentMask()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.help->{
+                val fragment = FragmentHelp()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        content = findViewById(R.id.frameLayout)
+
+        val navigation = findViewById<BottomNavigationView>(R.id.navigationView)
+        navigation.setOnNavigationItemSelectedListener(ItemSelectedListener)
+
+        val fragment = FragmentKorea.Companion.newInstance()
+        addFragment(fragment)
+
+    }
+
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            //.setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .replace(R.id.frameLayout, fragment, fragment.javaClass.simpleName)
+            .commit()
+    }
+}
+
+
+
+
+
+
