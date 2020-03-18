01f05df60c694bf8eddcd86913930adf64c25141
seounjin <invanda7@gmail.com>
2020-03-18 15:36:35 +0900

    크롤링,arrlist에 크롤링한 데이터 add

--- app/src/main/java/com/example/coronaapp/FragmentMask.kt
+++ app/src/main/java/com/example/coronaapp/FragmentMask.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 class FragmentMask : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
         return inflater.inflate(R.layout.fragment_mask, container, false)
     }
 
