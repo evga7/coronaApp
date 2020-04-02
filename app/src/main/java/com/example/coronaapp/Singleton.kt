edd103eacfe24a71e416f153916b314f80629a71
seounjin <invanda7@gmail.com>
2020-04-02 16:41:06 +0900

    타이틀에 이미지 추

--- app/src/main/java/com/example/coronaapp/Singleton.kt
+++ app/src/main/java/com/example/coronaapp/Singleton.kt
@@ -5,7 +5,7 @@ import com.example.coronaapp.world.Information
 
 class Singleton {
     companion object {
-        var     coronaList: ArrayList<Information>? = null
+        var coronaList: ArrayList<Information>? = null
         var coronaFlag:Boolean = false
         var countrySum:String? = null
         var totalCasesSum:String? = null
